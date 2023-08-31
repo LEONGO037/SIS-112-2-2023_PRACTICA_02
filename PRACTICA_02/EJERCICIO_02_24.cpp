24760e3c453ed7c8d1929f7764ef74f8c7f6bf6f
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2023-08-30 21:07:25 -0400

    Update EJERCICIO_02_24.cpp
    correccion del ejercicio 24

--- PRACTICA_02/EJERCICIO_02_24.cpp
+++ PRACTICA_02/EJERCICIO_02_24.cpp
@@ -1,11 +1,12 @@
 // Materia: Programacion I, Paralelo 4
 // Autor: Leonardo Delgado Medrano
 // Fecha creacion: 29/08/2023
-// Fecha modificacion: 29/08/2023
+// Fecha modificacion: 30/08/2023
 // Numero de ejericio: 24
 // Problema planteado: Leer una cadena en mayúsculas y cámbielas en mayúsculas.
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 int main()
 {
@@ -16,14 +17,18 @@ int main()
     for(char c:cadena)
     {
         //Valida que solo sean letras y que todas sean mayusculas
-        if(!isupper(c) && !isalpha(c))
+        if(!isupper(c) || !isalpha(c))
         {
             Mayuscula=false;
             break;
         }
     }
     if (Mayuscula)
-        cout<<"La cadena en solo mayusculas es"<<endl<<cadena<<endl;
+    {
+        //Codigo que convierte de mayuscula a minuscula
+        std::transform(cadena.begin(), cadena.end(), cadena.begin(), ::tolower);
+        cout<<"La cadena en solo minusculas es:"<<endl<<cadena<<endl;
+    }
     else
         cout<<"La cadena tiene al menos un caracter minuscula o tiene otros caracteres que no son letras"<<endl;
     return 0;
