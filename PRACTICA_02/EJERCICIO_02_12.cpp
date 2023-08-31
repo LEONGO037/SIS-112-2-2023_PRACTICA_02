16ac6bcdd06e9eecc2324e9288cf2c2414e30f4f
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2023-08-31 11:41:33 -0400

    Update EJERCICIO_02_12.cpp

--- PRACTICA_02/EJERCICIO_02_12.cpp
+++ PRACTICA_02/EJERCICIO_02_12.cpp
@@ -1,7 +1,7 @@
 // Materia: Programacion I, Paralelo 4
 // Autor: Leonardo Delgado Medrano
 // Fecha creacion: 29/08/2023
-// Fecha modificacion: 29/08/2023
+// Fecha modificacion: 31/08/2023
 // Numero de ejericio: 12
 // Problema planteado: Un arreglo constante contiene la producción en toneladas métricas de 6 minerales, y otro
 // contiene los nombres de estos minerales, para obtener
@@ -13,7 +13,13 @@ using namespace std;
 int main()
 {
     string mineral[6]={"SN", "SB", "AU", "PT", "AG", "CU"};
-    int toneladas[6]={998000, 876500, 786670, 636143, 135567, 109412};
+    int toneladas[6];
+    cout<<"Ingrese las toneladas metricas de cada mineral:"<<endl;
+    for (int i=0;i<6;i++)
+    {
+        cout<<"Del mineral "<<mineral[i]<<" ";
+        cin>>toneladas[i];
+    }
     string busqueda;
     cout<<"Mineral que desea buscar"<<endl;cin>>busqueda;
     for(int i=0;i<6;i++)
@@ -25,7 +31,7 @@ int main()
     {
         for(int j=0;j<6-i;j++)
         {
-            if(toneladas[j]>toneladas[j+1])
+            if(toneladas[j]<toneladas[j+1])
             {
                 int aux=toneladas[j];
                 toneladas[j]=toneladas[j+1];
